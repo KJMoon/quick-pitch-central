8f2a29c15379196d8edf5dd05e81edf9f1f2c4a7
Eric Moon <moonkangjin9195@gmail.com>
2022-10-11 14:11:20 -0400

    initial commit

---
+++ controllers/comments.js
@@ -0,0 +1,44 @@
+const Comment = require("../models/Comment");
+
+module.exports = {
+  createComment: async (req, res) => {
+    try {
+      // const commentUser = await User.findById(req.user.id);
+      await Comment.create({
+        comment: req.body.comment,
+        likes: 0,
+        post: req.params.id,
+        // createdBy: commentUser.userName,
+        createdBy: req.user.userName,
+        createdById: req.user.id,
+      });
+      console.log("Comment has been added!");
+      res.redirect("/post/"+req.params.id);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  likeComment: async (req, res) => {
+    try {
+      await Comment.findOneAndUpdate(
+        { _id: req.params.commentid },
+        {
+          $inc: { likes: 1 },
+        }
+      );
+      console.log("Likes +1");
+      res.redirect("/post/"+req.params.postid);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  deleteComment: async (req, res) => {
+    try {
+      await Comment.deleteOne({ _id: req.params.commentid });
+      console.log("Deleted Comment");
+      res.redirect("/post/"+req.params.postid);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
